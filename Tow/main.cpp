72aebc480f84c0c05bfd257caebe8e64f618ff5a
Saurabh <g03tiwari@gmail.com>
2023-05-14 22:42:14 +0530

    first commit

---
+++ Tow/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+template <class T1, class T2>
+class Complex
+{
+    T1 real;
+    T2 imaginary;
+
+public:
+    Complex(T1 r = 0, T2 i = 0)
+    {
+        this->real = r;
+        this->imaginary = i;
+    }
+
+    void print()
+    {
+        cout << "Complex Number: ";
+        cout << real << " + i" << imaginary << endl;
+    }
+};
+
+int main()
+{
+    Complex<int, int> c1(10, 9);
+    c1.print();
+    Complex<float, float> c2(10.5, 9.5);
+    c2.print();
+    Complex<int, float> c3(10, 9.5);
+    c3.print();
+    return 0;
+}
